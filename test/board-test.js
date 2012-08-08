6cb7750b3ebc7e9c92be04d42ae88aca6127dfc0
Merlyn Albery-Speyer <merlyn@newrelic.com>
2012-08-07 23:03:59 -0700

    Added index to move notation resolution, made index format the default for legal moves

--- test/board-test.js
+++ test/board-test.js
@@ -12,26 +12,36 @@ vows.describe('board').addBatch({
     topic: new Board(3),
 
     'should have 9 legal moves': function (board) {
-      assert.equal(board.legalMoves().length, 9, board.legalMoves().toString());
+      assert.equal(board.legalMoves().length, 9, board.notate(board.legalMoves()).toString());
     },
 
     'should have expected move notation': function (board) {
-      assert.ok(board.legalMoves().indexOf('A1') != -1, board.legalMoves().toString());
-      assert.ok(board.legalMoves().indexOf('C3') != -1, board.legalMoves().toString());
-    }
+      assert.ok(board.notate(board.legalMoves()).indexOf('A1') != -1, board.notate(board.legalMoves()).toString());
+      assert.ok(board.notate(board.legalMoves()).indexOf('C3') != -1, board.notate(board.legalMoves()).toString());
+    },
+
+    'should notate an index correctly': function(board) {
+      assert.equal(board.notate((5*1)+1+0), 'A1');
+      assert.equal(board.notate((5*1)+1+1), 'B1');
+      assert.equal(board.notate((5*2)+1+0), 'A2');
+    },
+
+    'should notate an array of indexes correctly': function(board) {
+      assert.deepEqual(board.notate([(5*1)+1+0, (5*1)+1+1, (5*2)+1+0]), ['A1', 'B1', 'A2']);
+    },    
   },
 
   'A 19x19 board': {
     topic: new Board(19),
 
     'should have 19^2 legal moves': function (board) {
-      assert.equal(board.legalMoves().length, 19*19, board.legalMoves().toString());
+      assert.equal(board.legalMoves().length, 19*19, board.notate(board.legalMoves()).toString());
     },
 
     'should have expected move notation': function (board) {
-      assert.ok(board.legalMoves().indexOf('A1') != -1, board.legalMoves().toString());
-      assert.ok(board.legalMoves().indexOf('T19') != -1, board.legalMoves().toString());
-      assert.ok(board.legalMoves().indexOf('I1') == -1, board.legalMoves().toString());
+      assert.ok(board.notate(board.legalMoves()).indexOf('A1') != -1, board.notate(board.legalMoves()).toString());
+      assert.ok(board.notate(board.legalMoves()).indexOf('T19') != -1, board.notate(board.legalMoves()).toString());
+      assert.ok(board.notate(board.legalMoves()).indexOf('I1') == -1, board.notate(board.legalMoves()).toString());
     }
   }
 }).exportTo(module);
