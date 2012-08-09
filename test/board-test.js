5be966d39331946f8cc2f80c868a6d13e438832f
Merlyn Albery-Speyer <merlyn@newrelic.com>
2012-08-08 23:12:46 -0700

    Added liberty counting for stone groups, and capturing on play

--- test/board-test.js
+++ test/board-test.js
@@ -9,7 +9,7 @@ var Board = require('./../lib/board.js');
 
 vows.describe('board').addBatch({
   'A 3x3 board': {
-    topic: new Board(3),
+    topic: function() { return Board(3) },
 
     'should have 9 legal moves': function (board) {
       assert.equal(board.legalMoves().length, 9, board.notate(board.legalMoves()).toString());
@@ -31,7 +31,7 @@ vows.describe('board').addBatch({
     },
 
     'should be black to play': function(board) {
-      assert.equal('b', board.notatePlayer(board.next()));
+      assert.equal(board.notatePlayer(board.next()), 'b');
     },
 
     'should reverse notate correctly': function(board) {
@@ -41,28 +41,52 @@ vows.describe('board').addBatch({
     },
 
     'after a tengen move': {
-      topic: function(board) { return board.play(board.next, board.reverseNotate('B2')); },
+      topic: function(board) { return Board(3).play(board.BLACK, board.reverseNotate('B2')); },
 
       'should have the correct liberty count': function(board) {
-        assert.equal(4, board.libertyCount(board.reverseNotate('B2')));
+        assert.equal(board.libertyCount(board.reverseNotate('B2')), 4);
       },
 
       'should be white to play': function(board) {
-        assert.equal('w', board.notatePlayer(board.next()));
+        assert.equal(board.notatePlayer(board.next()), 'w');
       },
 
       'after another move by black that connects': {
-        topic: function(board) { return board.play(board.next, board.reverseNotate('B1')); },
+        topic: function(board) { return board.play(board.BLACK, board.reverseNotate('B1')); },
 
         'should have the correct liberty count': function(board) {
           assert.equal(board.libertyCount(board.reverseNotate('B2')), 5);
+        }
+      }
+    },
+
+    'after a corner move by black': {
+      topic: function(board) { return Board(3).play(board.BLACK, board.reverseNotate('A1')); },
+
+      'should describe black as present in the corner': function(board) {
+        assert.equal(board.describe(board.reverseNotate('A1')), 'X');
+      },
+
+      'white takes the first liberty': {
+        topic: function(board) { return board.play(board.WHITE, board.reverseNotate('A2')); },        
+
+        'should describe black as present in the corner': function(board) {
+          assert.equal(board.describe(board.reverseNotate('A1')), 'X');
+        },
+
+        'white takes the second and final liberty': {
+          topic: function(board) { return board.play(board.WHITE, board.reverseNotate('B1')); },                  
+
+          'should describe black as captured in the corner': function(board) {
+            assert.equal(board.describe(board.reverseNotate('A1')), '.');
+          }
         }
       }
     }    
   },
 
   'A 19x19 board': {
-    topic: new Board(19),
+    topic: Board(19),
 
     'should have 19^2 legal moves': function (board) {
       assert.equal(board.legalMoves().length, 19*19, board.notate(board.legalMoves()).toString());
